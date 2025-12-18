52c3a757caf75b427236aab509f1218a4c0157a6
Navya1-hash <241801360008@cutmap.ac.in>
2025-12-18 14:16:01 +0530

    Update problem1.java

--- Module1_Array/problem1.java
+++ Module1_Array/problem1.java
@@ -1,1 +1,31 @@
-code
+ Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        
+        
+        // static D & I
+        int[] num = {1,2,3};
+        for(int i=0;i<num.length;i++){
+            System.out.println(num[i]);
+        }
+        
+        String[] names = {"jeet","raj","sunil"};
+        for(int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+        
+        // new
+         int[] num1 = new int[] {4,5,6};
+        for(int i=0;i<num1.length;i++){
+            System.out.println(num1[i]);
+        }
+        
+        String[] names1 = new String[] {"J","raj","sunil"};
+        for(int i=0;i<names1.length;i++){
+            System.out.println(names1[i]);
+        }
+        
+    }
+}
